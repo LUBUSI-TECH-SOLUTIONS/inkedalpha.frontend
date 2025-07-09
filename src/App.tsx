0ca76757a5a53abc955d3ef2fe1e62e05f1c22bf
Alejandro-RECO <revolledocoronado@hotmail.com>
2025-07-08 19:00:45 -0500

    feat(app): Add animate in logo and text

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 export const App = () => {
   return (
     <section className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <img src="/logos/Severo.webp" alt="Logo inkedalpha" loading="lazy"/>
-      <h1 className="text-2xl font-family-special">Coming soon...</h1>
+      <img className="animate-pulse" src="/logos/Severo.webp" alt="Logo inkedalpha" loading="lazy"/>
+      <h1 className="text-2xl font-family-special animate-pulse">Coming soon...</h1>
     </section>
   )
 }
